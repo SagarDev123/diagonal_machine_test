3f64654fd641e8519f1766381153de951df6e760
sagar <sagar@revyrieglobal.com>
2021-10-01 22:36:59 +0530

    HockeyApp sdk integrated

--- app/src/main/java/com/android/app/machinetestdiagonal/utils/MachineTestApp.kt
+++ app/src/main/java/com/android/app/machinetestdiagonal/utils/MachineTestApp.kt
@@ -1,11 +1,18 @@
 package com.android.app.machinetestdiagonal.utils
 
 import android.app.Application
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.AppCenter
 
 class MachineTestApp:Application() {
     override fun onCreate() {
         super.onCreate()
          instance = this
+        AppCenter.start(
+           this, "876cdc0d-6f87-4b31-b62b-cb30dd96ffd9",
+            Analytics::class.java, Crashes::class.java
+        )
     }
     companion object{
         var instance :MachineTestApp? =null
